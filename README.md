f8cff142895f8aa8e2b450fa532acd7e546e63de
cmw5630 <cmw5630@naver.com>
2019-02-08 17:08:38 +0900

    111

---
+++ README.md
@@ -0,0 +1,5 @@
+= Learning JavaScript, 3rd Edition
+== Chapter 2: JavaScript DeveLopment Tools
+
+In this chapter we're learning about Git and other
+development tools.
---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	'user strict';
+	paper.install(window);
+	paper.setup(document.getElementById('mainCanvas'));
+
+	var c = Shape.Circle(200, 200, 50);
+	c.fillColor = 'green';
+	paper.view.draw();
+	console.log('main.js loaded');
+});
